f0defac0ebe625ad224a17be6565f0b524fc5c4f
Nasrul Fuad <nasrulfuad08@gmail.com>
2022-04-23 16:21:50 +0700

    init server

---
+++ pdp/src/components/SafeComponent.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+class SafeComponent extends React.Component<unknown, { hasError: boolean }> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default SafeComponent;
